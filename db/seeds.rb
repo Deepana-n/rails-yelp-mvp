4ecd513714de7545e757f5432c20785e0fdb26bd
Deepana Naidu <deepana.naidu@gmail.com>
2021-02-18 13:54:20 +0800

    completed routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Start seeding"
+
+puts "Cleanup 🧹"
+Restaurant.destroy_all
+
+10.times do |index|
+  puts "Creating new restaurant 🚜"
+  resto = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: %w(chinese italian japanese french belgian).sample
+  )
+
+  3.times do
+    puts "Creating new review for #{resto.name} 🚜"
+    Review.create(
+      rating: (1..5).to_a.sample,
+      content: Faker::Lorem.paragraph,
+      restaurant: resto
+    )
+  end
+end
+
+puts "Done!"
